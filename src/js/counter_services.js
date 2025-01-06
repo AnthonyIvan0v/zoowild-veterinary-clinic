0c9098955562e0cf38a4f6c3ff4c80c739121dd9
AnthonyIvan0v <anton.ivanov.se@gmail.com>
2025-01-07 00:43:20 +0200

    прописує та підлючає код лічильника

---
+++ src/js/counter_services.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const counterElements = document.querySelectorAll('.number');
+    counterElements.forEach(el => {
+        el.originalValue = parseInt(el.textContent.replace(/\s+/g, ''), 10); // Сохранение оригинального значения
+        el.textContent = '0'; // Установка начального значения в ноль
+    });
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const element = entry.target;
+                    const targetNumber = element.originalValue;
+                    setTimeout(() => startCounter(element, targetNumber), 500);
+                    observer.unobserve(element);
+                }
+            });
+        },
+        {
+            threshold: 0.1,
+        }
+    );
+    counterElements.forEach(el => {
+        observer.observe(el);
+    });
+    function startCounter(element, target) {
+        const duration = 1000; // Длительность анимации в миллисекундах
+        const increment = target / (duration / 16); // Обновление каждые 16 мс (приблизительно 60 fps)
+        let current = 0;
+        function updateCounter() {
+            current += increment;
+            if (current < target) {
+                element.textContent = Math.ceil(current).toLocaleString();
+                requestAnimationFrame(updateCounter);
+            } else element.textContent = target.toLocaleString();
+        }
+        updateCounter();
+    }
+});
