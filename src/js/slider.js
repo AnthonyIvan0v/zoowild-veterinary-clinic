666980de3add055501911721486e0550c7848805
AnthonyIvan0v <anton.ivanov.se@gmail.com>
2025-01-06 14:54:54 +0200

    додає блок пагінації та стилі до нього

--- src/js/slider.js
+++ src/js/slider.js
@@ -18,8 +18,27 @@ const cubeSlider = new Swiper('.cube-slider', {
     },
     pagination: {
         el: '.swiper-pagination',
+        type: 'bullets',
         clickable: true,
         dynamicMainBullets: 4,
+        currentClass: '.active',
+        totalClass: '.swiper-pagination-total',
+    },
+
+    on: {
+        init: function () {},
+        slideChange: function () {
+            const swiper = this;
+            const currentSlide = document.querySelector(
+                '.swiper-pagination-total'
+            );
+            const slideNumber = (swiper.realIndex + 1)
+                .toString()
+                .padStart(2, '0');
+            if (currentSlide) {
+                currentSlide.textContent = slideNumber;
+            }
+        },
     },
 });
 
