fcaefa776d2af381e7f3041496e33e0fabfda185
AnthonyIvan0v <anton.ivanov.se@gmail.com>
2025-02-03 11:20:39 +0200

    додає class is-hidden на модалку сертифікатів

--- src/js/openModaWindowLicenes.js
+++ src/js/openModaWindowLicenes.js
@@ -4,6 +4,7 @@
             '[data-open-foto-licenses]'
         ),
         closeFotoLicensesBTN: document.querySelector('[data-close-licenses]'),
+        modalLicenses: document.querySelector('[data-modal-foto-licenses]'),
         thumbFotoLicenses: document.querySelector('[data-thumb-foto]'),
         imgModalLicenses: document.querySelector('[data-img-modal-lisenses]'),
         backdrop: document.querySelector('.backdrop'),
@@ -20,6 +21,7 @@
             const srcImg = event.currentTarget.getAttribute('src');
             refs.imgModalLicenses.src = srcImg;
         }
+        refs.modalLicenses.classList.toggle('is-hidden');
         refs.backdrop.classList.toggle('is-hidden');
         document.body.classList.toggle('not-scroll');
     }
