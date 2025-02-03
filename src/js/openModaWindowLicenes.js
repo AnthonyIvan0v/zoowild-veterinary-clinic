2f8f121c46ef0dd90f893194e8327fa657c50daa
AnthonyIvan0v <anton.ivanov.se@gmail.com>
2025-02-03 10:59:08 +0200

    дописує функціонал відкриття модального вікна з сертифікатами

--- src/js/openModaWindowLicenes.js
+++ src/js/openModaWindowLicenes.js
@@ -5,32 +5,20 @@
         ),
         closeFotoLicensesBTN: document.querySelector('[data-close-licenses]'),
         thumbFotoLicenses: document.querySelector('[data-thumb-foto]'),
+        imgModalLicenses: document.querySelector('[data-img-modal-lisenses]'),
         backdrop: document.querySelector('.backdrop'),
     };
 
-    refs.openFotoLicenses.forEach(elem =>
-        elem.addEventListener('click', toggleModalLicenses)
-    );
     refs.closeFotoLicensesBTN.addEventListener('click', toggleModalLicenses);
 
+    refs.openFotoLicenses.forEach(img => {
+        img.addEventListener('click', toggleModalLicenses);
+    });
+
     function toggleModalLicenses(event) {
-        const typelicenses = event.currentTarget.getAttribute('licenses');
-        console.log(typelicenses);
-        switch (typelicenses) {
-            case 'diploma':
-                // refs.thumbFotoLicenses.innerHTML = `<img src="./images/content-images/licenses/diploma_1920.jpg"/>`;
-                refs.thumbFotoLicenses.innerHTML = `<img src="/images/content-images/licenses/diploma_1920@2x.jpg"/>`;
-                console.log(typelicenses);
-                break;
-            case 'certificate':
-                refs.thumbFotoLicenses.innerHTML = `<img src="./images/content-images/licenses/certificate_1920@2x.jpg"/>`;
-                break;
-            case 'second_diploma':
-                refs.thumbFotoLicenses.innerHTML = `<img src="./images/content-images/licenses/second_diploma_1920@2x.jpg"/>`;
-                break;
-                refs.thumbFotoLicenses.innerHTML = '';
-            default:
-            // }
+        if (event.currentTarget.getAttribute('src')) {
+            const srcImg = event.currentTarget.getAttribute('src');
+            refs.imgModalLicenses.src = srcImg;
         }
         refs.backdrop.classList.toggle('is-hidden');
         document.body.classList.toggle('not-scroll');
