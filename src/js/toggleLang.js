2922b3c0e7a5f4d93554c6feb0cb315a11ef0350
AnthonyIvan0v <anton.ivanov.se@gmail.com>
2025-02-12 14:58:43 +0200

    прописує логіку на кнопку перемикання мови

--- src/js/toggleLang.js
+++ src/js/toggleLang.js
@@ -1,16 +1,30 @@
-(() => {
-    const refs = {
-        toggleLangBtn: document.querySelectorAll('[data-toggle-lang]'),
-        swithcer: document.querySelectorAll('.switch_lang'),
-        spanUA: document.querySelectorAll('.lang_ua'),
-        spanRU: document.querySelectorAll('.lang_ru'),
-    };
+import { refs } from './refs';
 
-    refs.toggleLangBtn.forEach(elem => elem.addEventListener('click', toggle));
+if (window.location.href.includes('#')) {
+    const index = window.location.href.indexOf('#');
+    const shortAddres = window.location.href.slice(0, index);
+    const modifiedAddres = shortAddres + '#ua';
+    window.location.href = modifiedAddres;
+} else {
+    window.location.href += '#ua';
+}
 
-    function toggle() {
-        refs.swithcer.forEach(elem => elem.classList.toggle('toggle'));
-        refs.spanUA.forEach(elem => elem.classList.toggle('inactive'));
-        refs.spanRU.forEach(elem => elem.classList.toggle('inactive'));
+refs.toggleLangBtn.forEach(elem => elem.addEventListener('click', toggle));
+
+function toggle() {
+    const modifiedHref = window.location.href.slice(
+        0,
+        window.location.href.length
+    );
+
+    refs.swithcer.forEach(elem => elem.classList.toggle('toggle'));
+    refs.spanUA.forEach(elem => elem.classList.toggle('inactive'));
+    refs.spanRU.forEach(elem => elem.classList.toggle('inactive'));
+    if (modifiedHref.includes('#ua')) {
+        const modifiedAddres = window.location.href.replace('#ua', '#ru');
+        window.location.href = modifiedAddres;
+    } else {
+        const modifiedAddres = window.location.href.replace('#ru', '#ua');
+        window.location.href = modifiedAddres;
     }
-})();
+}
